17c307247fa7cac2fa5dc452ef91f242d05d44ab
Kai Sano <thinhddo.vn@gmail.com>
2021-08-23 21:52:41 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 baguetteBox.run('.gallery');
 
 const search = new Filter('search', 'data-caption');
+
+// var input, filter, search, gallery;
+// input = document.getElementById("search").addEventListener("keyup", myFuction);
+// filter = input.value.toUpperCase();
+// search = document.getElementsById("gallery");
+// gallery = search.getElementsByTagName("a");
+
+// function myFuction(){
+//     var i = document.getElementById("search");
+//     i.value = i.value.toLowerCase();
+    
+
